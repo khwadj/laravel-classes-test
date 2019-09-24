c8362b788508ee92178737e386a9c5fa3a6147ca
Paoulo RIVEROS <priv@igm3.priv>
2019-09-24 14:19:53 +0200

    title

--- resources/views/eloquent/index.blade.php
+++ resources/views/eloquent/index.blade.php
@@ -2,27 +2,31 @@
 
 @section('content')
 
-    <ul>
-        <li>
-            <a href=" {{ route('eloquent_collection') }}">
-                Collection tests
-            </a>
-        </li>
-        <li>
-            <a href=" {{ route('eloquent_cache') }}">
-                Cache tests
-            </a>
-        </li>
-        <li>
-            <a href=" {{ route('eloquent_model') }}">
-                Model tests
-            </a>
-        </li>
-        <li>
-            <a href=" {{ route('eloquent_builder') }}">
-                Builder tests
-            </a>
-        </li>
-    </ul>
+    <h1 class="col-12">Eloquent tests</h1>
+
+    <div class="col-12">
+        <ul>
+            <li>
+                <a href=" {{ route('eloquent_collection') }}">
+                    Collection tests
+                </a>
+            </li>
+            <li>
+                <a href=" {{ route('eloquent_cache') }}">
+                    Cache tests
+                </a>
+            </li>
+            <li>
+                <a href=" {{ route('eloquent_model') }}">
+                    Model tests
+                </a>
+            </li>
+            <li>
+                <a href=" {{ route('eloquent_builder') }}">
+                    Builder tests
+                </a>
+            </li>
+        </ul>
+    </div>
 
 @endsection
